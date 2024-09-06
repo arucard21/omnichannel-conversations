aaba0a5cd21d4dd0cf20ec2918f9c870c075957f
arucard21 <arucard21@gmail.com>
2024-09-06 14:23:35 +0200

    Add configuration steps for installing Vitess.

--- configure_k8s.sh
+++ configure_k8s.sh
@@ -6,9 +6,9 @@ kubectl create namespace integrations
 # Install Camel K
 helm install camel-k --set platform.build.registry.address=registry:5000 camel-k/camel-k --namespace integrations
 ## Testing Camel K
-kamel run test.yaml --dev --namespace integrations
+kamel run kamel-test-integration.yaml --dev --namespace integrations
 
-# Install Strimzi Operator
+# Install Strimzi Operator (helm version must be at least 3.8 to use OCI registry)
 helm install strimzi-cluster-operator oci://quay.io/strimzi-helm/strimzi-kafka-operator --namespace integrations
 # Create a Kafka cluster
 kubectl apply -f kafka-cluster-single-node.yaml --namespace integrations
@@ -18,3 +18,10 @@ kubectl apply -f kafka-cluster-single-node.yaml --namespace integrations
 #kubectl -n integrations run kafka-producer -ti --image=quay.io/strimzi/kafka:0.43.0-kafka-3.8.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --bootstrap-server kafka-omnichannel-conversations-kafka-bootstrap:9092 --topic omnichannel.conversations
 # Receive messages sent to Kafka topic
 #kubectl -n integrations run kafka-consumer -ti --image=quay.io/strimzi/kafka:0.43.0-kafka-3.8.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server kafka-omnichannel-conversations-kafka-bootstrap:9092 --topic omnichannel.conversations --from-beginning
+
+# Create namespace to use specifically for data persistence
+kubectl create namespace datastore
+# Install Vitess Operator (including CRDs)
+kubectl apply -f vitess-operator.yaml --namespace datastore
+# Create a minimal cluster for Vitess distributed database
+kubectl apply -f vitess-minimal-cluster.yaml --namespace datastore
